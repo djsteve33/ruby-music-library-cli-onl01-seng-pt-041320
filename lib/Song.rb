cd99477fab6224011be2f483bbe6411634ddb53a
Steve Womble <swomble@aol.com>
2020-06-12 21:25:58 +0000

    Automatically backed up by Learn

--- lib/Song.rb
+++ lib/Song.rb
@@ -10,7 +10,6 @@ class Song
   
   def initialize(name, artist = nil, genre = nil)
     @name = name
-    
     self.artist=(artist) if artist != nil
     self.genre=(genre) if genre != nil
     end
